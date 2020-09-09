f7303d1a2ab1d7fd5f1ab800601e17e6bf35d962
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-09-10 01:47:46 +0300

    старт

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Artem", 20, 3);
+            s1.SetMarks();
+            s1.ShowInfo();
+            Console.WriteLine(s1.GetAvgMark());
+            s1.ResetAllMarks();
+            s1.ShowInfo();
+        }
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Task1
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int NumberOfSubjects { get; set; }
+        private Mark[] marks;
+
+        public Student()
+        {
+            Name = "";
+            Age = 0;
+            NumberOfSubjects = 0;
+            marks = null;
+        }
+        public Student(string n, int a, int N)
+        {
+            Name = n;
+            Age = a;
+            NumberOfSubjects = N;
+            marks = new Mark[NumberOfSubjects];
+            for (int i = 0; i < marks.Length; i++)
+                marks[i] = new Mark();
+        }
+        public void SetMarks()
+        {
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.Write("enter subject: ");
+                marks[i].Subject = Console.ReadLine();
+                Console.Write("enter value: ");
+                marks[i].Value = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("name: "+Name+" age: "+Age+" NumberOfMarks: "+ NumberOfSubjects);
+            if(marks != null)
+                for (int i = 0; i < marks.Length; i++)
+                    marks[i].ShowInfo();
+        }
+        public int GetAvgMark()
+        {
+            if(marks.Length!=0)
+            {
+                int sum = 0;
+                for (int i = 0; i < marks.Length; i++)
+                    sum += marks[i].Value;
+                return (int)Math.Round((double)(sum / marks.Length));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public void ResetAllMarks()
+        {
+            for (int i = 0; i < marks.Length; i++)
+                marks[i].Value = 0;
+        }
+    }
+}
